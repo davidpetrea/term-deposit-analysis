898561fecd0e3c63561ff96a51eef795fea30095
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2023-05-27 19:05:15 +0300

    Initial preprocessing, feature adjustments and sampling.

---
+++ wald_test_R.R
@@ -0,0 +1,20 @@
+
+library(tidyr)
+library(tidyverse)
+
+getwd()
+setwd("C:/Users/wyver/Desktop/deposit-analysis")
+
+
+data = read.csv("data/selected_data.csv")
+
+
+data$education <- as.factor(data$education)
+data$subscribed <- as.factor(data$subscribed)
+
+#fit regression model
+model <- glm(subscribed ~ education, data = data, family = 'binomial')
+
+#view model summary
+summary(model)
+
